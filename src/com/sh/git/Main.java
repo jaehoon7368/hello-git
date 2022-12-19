f4f209be0af0b407f03cce726589dc072780d475
jaehoon7368 <dbwogns7368@naver.com>
2022-12-19 11:20:52 +0900

    reset이후 세로운 버젼

--- src/com/sh/git/Main.java
+++ src/com/sh/git/Main.java
@@ -1,5 +1,17 @@
 package com.sh.git;
-
+/*
+ * 되돌리기
+ *  -revert : 지정한 커밋의 작업내용을 취소한 버젼을 새로운 커밋으로 만든다.(conflict날수 있음)
+ *  
+ *  -reset : 지정한 커밋으로 돌아간다.
+ *    - hard : 지정한 커밋 이후 변경사항 버림
+ *    - mixed : 지정 커밋 이후 변경사항을 unstaging area 보관. 재추가후 커밋 가능
+ *    - soft : 지정 커밋 이후 변경사항을 staging area 보관. 즉시 커밋 가능
+ */
 public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
 
+	}
 }
